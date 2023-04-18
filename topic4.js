92149d8f0615a6642309f90d327081b0b2f4e84c
masud_osman <masud@groots.co.jp>
2023-04-18 14:45:02 +0600

    topic 4 fourth commit

--- topic4.js
+++ topic4.js
@@ -4,3 +4,5 @@ console.log('this is topic 4 1st console');
 console.log('this is topic 4 second console');
 //third commit
 console.log('this is topic 4 third console');
+//fourth commit
+console.log('this is topic 4 fourth console');
